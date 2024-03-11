18f576f2431a34d4fcee7ffe823404310bdbd3fd
YasineSouissi <yasinesouissi@gmail.com>
2024-03-11 18:11:46 -0400

    fesh push

---
+++ itis3135/scripts/calculator_try.js
@@ -0,0 +1,14 @@
+const calcholder =document.getElementById('calculator');
+const display =document.getElementById('display');
+const addition =document.getElementById('plus');
+const subtraction =document.getElementById('minus');
+const multiplication =document.getElementById('multiply');
+const division =document.getElementById('divide');
+const equal =document.getElementById('equal');
+
+const zero =document.getElementById('zero');
+const one =document.getElementById('one');
+const two =document.getElementById('two');
+const three =document.getElementById('three');
+const four =document.getElementById('four');
+
